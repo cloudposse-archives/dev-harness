911c58ce183dc3ae40584efcd07a1aaf82ea4e8a
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-24 11:18:45 +0300

    Add script which updatea terraform submodules. Update all submodules

---
+++ scripts/update-submodules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f scripts/cache.txt ]; 
+then
+    repos=$(make git/list-repos | grep '^terraform-')
+    echo "$repos" > scripts/cache.txt
+else
+    repos=$(<scripts/cache.txt)
+fi
+
+COUNTER=0
+for repo in $repos; do
+  if [ ! -d "projects/terraform/${repo}" ]; then
+    git submodule add "git@github.com:cloudposse/$repo.git" "projects/terraform/$repo"
+    (( COUNTER++ ))
+  fi
+done
+echo "Count of new submodules: $COUNTER"
